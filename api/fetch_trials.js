742048dc7a6f789ee7701ff682087c228fac96c4
Aadi Yadav <68039511+EunoiaC@users.noreply.github.com>
2025-08-09 15:46:00 -0700

    Update fetch_trials.js

--- api/fetch_trials.js
+++ api/fetch_trials.js
@@ -9,9 +9,8 @@ const headers = {
 
 export default async function fetch_trials(req, res) {
     try {
-        const { codes } = req.body;
+        const { ids } = req.body;
         // split by comma and make a list of ids using codes
-        const ids = codes.split(',').map(code => code.trim());
         const search_url = `${BASE_URL}/trials`;
 
         let processed_data = [];
